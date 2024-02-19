cb55f1a959fdfd5be29f114ee9612baf6d5623cb
FelixLuciano <felix.lucianodias@gmail.com>
2024-02-18 22:53:45 -0300

    Add test cases for test_main.py

--- test/test_main.py
+++ test/test_main.py
@@ -1,9 +1,21 @@
-from src.main import result
+from test.utils import run_testcase
 
 
-def test_result():
-    assert result() == True, "Hello World!"
+def test_testcase_0():
+    run_testcase("data/testcase_0.txt")
 
+def test_testcase_1():
+    failed = False
 
-def test_result2():
-    assert result() != False, "Hello World!"
+    try:
+        run_testcase("data/testcase_1.txt")
+    except Exception:
+        failed = True
+
+    assert failed, "Target is out of bounds. Should've failed!"
+
+def test_testcase_2():
+    run_testcase("data/testcase_2.txt")
+
+def test_testcase_3():
+    run_testcase("data/testcase_3.txt")
